bacc8d51aedc7649b347d180104be868ed3efd00
Bestin-07 <bestinantunps@gmail.com>
2022-08-03 09:58:34 +0530

    PROCESS

--- wowow.py
+++ wowow.py
@@ -103,7 +103,7 @@ class EGM(object):
         if joint_angles is not None:
             #joint_angles2 = list(np.rad2deg(joint_angles))
             #print(joint_angles2)
-            planned.joints.joints[:] = [0.0,0.0,0.0,180,0.0,0.0]
+            planned.joints.joints[:] = joint_angles
             print(planned.joints.joints)
         buf2=sensorMessage.SerializeToString()
 
@@ -149,98 +149,96 @@ class EGM(object):
 ##############################################################################################
 
 
-def Vedio_Process(rob_joints):
-    while(cap.isOpened()):
-        ret, frame = cap.read()
-        if ret == False:
-            break
-        img_path=frame
-        img=img_path.copy()
-        org = img_path.copy()
-        gray_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-        blur_img = cv2.GaussianBlur(gray_img,(3,3),0)
-        edged_img = cv2.Canny(blur_img,0,200)
-        cnts,_ = cv2.findContours(edged_img.copy(),cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
-        cnts = sorted(cnts,key=cv2.contourArea,reverse=True)[:5]
-        for c in cnts:
-            peri = cv2.arcLength(c,True)
-            approx = cv2.approxPolyDP(c,0.02*peri,True)
-            if len(approx)==4:
-                doc = approx
-                break
-        p=[]
-        for d in doc:
-            tuple_point = tuple(d[0])
-            cv2.circle(img,tuple_point,3,(0,0,255),4)
-            p.append(tuple_point)
-        warped = four_point_transform(org, doc.reshape(4, 2))
-        img1=cv2.resize(warped, (600, 800))
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        new_frame_time=time.time()
-        fps = 1/(new_frame_time-prev_frame_time)
-        prev_frame_time = new_frame_time
-        fps = int(fps)
-        cv2.imshow('frame', img1 )
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-        image=img1
-        output=image.copy()
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        gray_blurred = cv2.blur(gray, (11, 11),10)
-        circles = cv2.HoughCircles(gray_blurred,cv2.HOUGH_GRADIENT, 1, 50, param1 = 50,param2 = 30, minRadius = 50, maxRadius = 150)
-        if circles is not None:
-            circles =np.round(circles[0, :]).astype("int")
-        circles[0][0]=circles[0][0]-300
-        circles[0][1]=circles[0][1]-400
-        print(circles)
-        circles[0][0]=circles[0][0]+300
-        circles[0][1]=circles[0][1]+400
-        for (x, y, r) in circles:
-            cv2.circle(output, (x, y), r, (0, 255, 0), 2)
-        if i>0:
-            t=float(1/fps)
-            print(t)
-            if(x<0):
-                x=(x+300)
-            elif(x>0):
-                x=x-300
-
-            if(y<0):
-                y=y+400
-            elif(y>0):
-                y=y-400
-            print(x,y) 
-            p=math.atan(y/x)
-            d= math.sqrt(x*x+y*y)
-            dx= d*(math.cos(p))
-            dy=d*(math.sin(p))
-            s=d/(100*t)
-            sx= s*(math.cos(p))
-            sy=s*(math.sin(p))
-            #a=float(d/800)
-            #b=float(s/2)
-            #q=float(a*b*45)
-            qx=(dx/400)*(sx/2)*45
-            qy=(dy/300)*(sy/2)*45
-
-            print(d,s,dx,dy,sx,sy,qx,qy)
-        i += 1
-    cap.release() 
-    cv2.waitKey(0)
-    cv2.destroyAllWindows() 
-
-
-
 EGM1 = EGM()
 
 while True:
     
     Current_Position = EGM1.receive_from_robot()[1]
     if Current_Position is not None:
-        while True:
-            #Current_Position = EGM1.receive_from_robot()[1]
-            #print(Current_Position)
-            EGM1.send_to_robot_joints(Current_Position)
-            time.sleep(.1)
+        while(cap.isOpened()):
+            ret, frame = cap.read()
+            if ret == False:
+                break
+            img_path=frame
+            img=img_path.copy()
+            org = img_path.copy()
+            gray_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+            blur_img = cv2.GaussianBlur(gray_img,(3,3),0)
+            edged_img = cv2.Canny(blur_img,0,200)
+            cnts,_ = cv2.findContours(edged_img.copy(),cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+            cnts = sorted(cnts,key=cv2.contourArea,reverse=True)[:5]
+            for c in cnts:
+                peri = cv2.arcLength(c,True)
+                approx = cv2.approxPolyDP(c,0.02*peri,True)
+                if len(approx)==4:
+                    doc = approx
+                    break
+            p=[]
+            for d in doc:
+                tuple_point = tuple(d[0])
+                cv2.circle(img,tuple_point,3,(0,0,255),4)
+                p.append(tuple_point)
+            warped = four_point_transform(org, doc.reshape(4, 2))
+            img1=cv2.resize(warped, (600, 800))
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            new_frame_time=time.time()
+            fps = 1/(new_frame_time-prev_frame_time)
+            prev_frame_time = new_frame_time
+            fps = int(fps)
+            cv2.imshow('frame', img1 )
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+            image=img1
+            output=image.copy()
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            gray_blurred = cv2.blur(gray, (11, 11),10)
+            circles = cv2.HoughCircles(gray_blurred,cv2.HOUGH_GRADIENT, 1, 50, param1 = 50,param2 = 30, minRadius = 50, maxRadius = 150)
+            if circles is not None:
+                circles =np.round(circles[0, :]).astype("int")
+            circles[0][0]=circles[0][0]-300
+            circles[0][1]=circles[0][1]-400
+            print(circles)
+            circles[0][0]=circles[0][0]+300
+            circles[0][1]=circles[0][1]+400
+            for (x, y, r) in circles:
+                cv2.circle(output, (x, y), r, (0, 255, 0), 2)
+            if i>0:
+                t=float(1/fps)
+                print(t)
+                if(x<0):
+                    x=(x+300)
+                elif(x>0):
+                    x=x-300
+
+                if(y<0):
+                    y=y+400
+                elif(y>0):
+                    y=y-400
+                print(x,y) 
+                p=math.atan(y/x)
+                d= math.sqrt(x*x+y*y)
+                dx= d*(math.cos(p))
+                dy=d*(math.sin(p))
+                s=d/(100*t)
+                sx= s*(math.cos(p))
+                sy=s*(math.sin(p))
+                #a=float(d/800)
+                #b=float(s/2)
+                #q=float(a*b*45)
+                qx=(dx/400)*(sx/2)*45
+                qy=(dy/300)*(sy/2)*45
+
+                print(d,s,dx,dy,sx,sy,qx,qy)
+            i += 1
+        
+        #Current_Position = EGM1.receive_from_robot()[1]
+        #print(Current_Position)
+        EGM1.send_to_robot_joints(Current_Position)
+        time.sleep(.1)    
+        
+        cap.release() 
+        cv2.waitKey(0)
+        cv2.destroyAllWindows() 
+
     else:
         print("Waiting for robot to connect")
